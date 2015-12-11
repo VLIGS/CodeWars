960159430a5e555e9298ed1b35842cd3af3ed1a3
VLIGS <liudmila.veshneva@gmail.com>
2015-12-11 20:32:17 +0000

    done

--- src/Arge.java
+++ src/Arge.java
@@ -2,8 +2,15 @@
 public class Arge {
 
     public static int nbYear(int p0, double percent, int aug, int p) {
-        // your code
-        return 0;
+        percent = percent/100;
+        int count = 0;
+        double populationIncrease;
+        while(p>=p0){
+            populationIncrease = (double)p0 * percent;
+            p0 = p0 + (int)populationIncrease + aug;
+            count+=1;
+        }
+        return count;
     }
 }
 
