71bc95179663c638c8c5bed58f13fe4893d86980
VLIGS <liudmila.veshneva@gmail.com>
2015-12-11 19:04:08 +0000

    first letter of sentence to capital

--- src/JadenCase.java
+++ src/JadenCase.java
@@ -1,10 +1,20 @@
 
 public class JadenCase {
 
-    // capitalize every word
+    // capitalize every word in phrase
     public String toJadenCase(String phrase) {
+        if (phrase.equals(null)){
+            return null;
+        }
+        StringBuilder newPhrase = new StringBuilder(phrase);
+        char capital = phrase.charAt(0);
+        if(!Character.isDigit(capital) && !Character.isWhitespace(capital)){
+            newPhrase.setCharAt(0, Character.toUpperCase(capital));
+        }
+        for(int i = 1; i<phrase.length();i++){
 
 
-        return null;
+        }
+        return newPhrase.toString();
     }
 }
