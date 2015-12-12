b990336ec40b581ebf80ce205cd701e1c5ddcb23
VLIGS <liudmila.veshneva@gmail.com>
2015-12-12 10:53:41 +0000

    second test passed

--- src/EANValidator.java
+++ src/EANValidator.java
@@ -1,5 +1,26 @@
 public class EANValidator {
     public static boolean validate(final String eanCode) {
-        return true;
+        int runningSum = 0;
+        int checkSum = 0;
+
+        for(int i = 0; i<eanCode.length()-1; i++){
+            if((i%2)==0){
+                runningSum = runningSum + eanCode.charAt(i) * 3;
+            }
+            else {
+                runningSum = runningSum + eanCode.charAt(i);
+            }
+        }
+
+        if((runningSum%10) != 0){
+            checkSum = 10 - runningSum%10;
+        }
+
+        if(checkSum == eanCode.charAt(eanCode.length()-1)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
