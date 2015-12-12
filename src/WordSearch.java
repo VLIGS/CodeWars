b4b9575c895683ce508f82cc0ed7912114292e14
VLIGS <liudmila.veshneva@gmail.com>
2015-12-12 08:57:13 +0000

    one test passed WordSearch

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -1,7 +1,22 @@
 class WordSearch {
     static String[] findWord(String x, String[] y){
-        
-        return new String[0];
+        int count=0;
+        for (int i = 0; i<y.length; i++)
+        {
+            if (y[i].contains(x)) {
+                count+=1;
+            }
+        }
+        String [] myReturnString = new String[count];
+        count=0;
+        for (int i = 0; i<y.length; i++)
+        {
+            if (y[i].contains(x)) {
+                myReturnString[count] = y[i];
+                count+=1;
+            }
+        }
+        return myReturnString;
     }
 }
 
