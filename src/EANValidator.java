9bfd628f146fc08db4562d7b5ad8b85d45589460
VLIGS <liudmila.veshneva@gmail.com>
2015-12-12 11:08:50 +0000

    woo hoo! 2nd  Kata submitted

--- src/EANValidator.java
+++ src/EANValidator.java
@@ -4,19 +4,19 @@ public class EANValidator {
         int checkSum = 0;
 
         for(int i = 0; i<eanCode.length()-1; i++){
-            if((i%2)==0){
-                runningSum = runningSum + eanCode.charAt(i) * 3;
+            String myCharacter = String.valueOf(eanCode.charAt(i));
+            if(((i+1)%2)==0){
+                runningSum = runningSum + Integer.parseInt(myCharacter) * 3;
             }
             else {
-                runningSum = runningSum + eanCode.charAt(i);
+                runningSum = runningSum + Integer.parseInt(myCharacter);
             }
         }
-
         if((runningSum%10) != 0){
             checkSum = 10 - runningSum%10;
         }
 
-        if(checkSum == eanCode.charAt(eanCode.length()-1)){
+        if(checkSum == Integer.parseInt(String.valueOf(eanCode.charAt(eanCode.length()-1)))){
             return true;
         }
         else{
