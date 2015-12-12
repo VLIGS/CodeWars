248956fc862a6d2ae41c7257d70d47f59c667491
VLIGS <liudmila.veshneva@gmail.com>
2015-12-12 18:01:18 +0000

    tests passed Kata submitted DnaStrand

--- src/DnaStrand.java
+++ src/DnaStrand.java
@@ -1,7 +1,21 @@
 
 public class DnaStrand {
     public static String makeComplement(String dna) {
-        return null;
+        String myReturnString = "";
+        for (int i = 0; i<dna.length(); i++){
+            switch (dna.charAt(i)){
+                case 'A': myReturnString = myReturnString + 'T';
+                    break;
+                case 'T': myReturnString = myReturnString + 'A';
+                    break;
+                case 'C': myReturnString = myReturnString + 'G';
+                    break;
+                case 'G': myReturnString = myReturnString + 'C';
+                    break;
+                default: myReturnString = myReturnString + "";
+            }
+        }
+        return myReturnString;
     }
 }
 
